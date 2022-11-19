746c0d4cf9cbf829d69fdeb4adc952603e86ba2b
nlog <nlog675@gmail.com>
2022-11-19 14:05:12 +0300

    add domain name

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -1,8 +1,6 @@
 const allowedCors = [
   'http://nlog675.nomoredomains.icu',
   'localhost:3000',
-  'https://localhost:3000',
-  'http://localhost:3000',
 ];
 
 const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
