14c181f1039ae386912364b6fdef43a0d13c056d
nlog <nlog675@gmail.com>
2022-11-20 23:53:23 +0300

    Revert "fix"
    This reverts commit 2fc4ef655b02ab8fbabfde30e923be2bd8462391.

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const token = require('jsonwebtoken');
 const UnauthorizedError = require('../utils/UnauthorizedError');
 
+// eslint-disable-next-line no-unused-vars
 const { NODE_ENV, JWT_SECRET } = process.env;
 
 // eslint-disable-next-line consistent-return
@@ -13,7 +14,7 @@ module.exports = (req, res, next) => {
   let payload;
 
   try {
-    payload = token.verify(jwt, NODE_ENV === 'production' ? JWT_SECRET : 'some-secret-key');
+    payload = token.verify(jwt, JWT_SECRET);
   } catch (err) {
     return next(new UnauthorizedError('Необходима авторизация'));
   }
