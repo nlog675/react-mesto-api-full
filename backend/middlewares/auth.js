00be175f83d3ecca27431c28138664c84a9fad37
nlog <nlog675@gmail.com>
2022-11-21 00:04:18 +0300

    try

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,9 +1,7 @@
 require('dotenv').config();
-
 const token = require('jsonwebtoken');
 const UnauthorizedError = require('../utils/UnauthorizedError');
 
-// eslint-disable-next-line no-unused-vars
 const { NODE_ENV, JWT_SECRET } = process.env;
 
 // eslint-disable-next-line consistent-return
@@ -15,7 +13,7 @@ module.exports = (req, res, next) => {
   let payload;
 
   try {
-    payload = token.verify(jwt, JWT_SECRET);
+    payload = token.verify(jwt, NODE_ENV === 'production' ? JWT_SECRET : 'some-secret-key');
   } catch (err) {
     return next(new UnauthorizedError('Необходима авторизация'));
   }
