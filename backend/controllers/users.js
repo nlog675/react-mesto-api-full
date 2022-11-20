6b9386c513c42adef8e53a894bc2edc88e5f0242
nlog <nlog675@gmail.com>
2022-11-20 15:16:33 +0300

    hotfix

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -1,5 +1,3 @@
-require('dotenv').config();
-
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
@@ -105,7 +103,6 @@ const login = (req, res, next) => {
             throw new UnauthorizedError('Неправильный логин или пароль');
           }
           const token = jwt.sign({ _id: user._id }, NODE_ENV === 'production' ? JWT_SECRET : 'some-secret-key', { expiresIn: '7d' });
-          console.log(NODE_ENV, JWT_SECRET);
           res.cookie('jwt', token, {
             maxAge: 3600000 * 24 * 7,
             httpOnly: true,
